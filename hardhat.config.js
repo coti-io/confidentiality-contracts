e7e77497914a39e29c12412b20137dbdd4131777
vladi-coti <vladi@coti.io>
2024-04-04 16:52:19 +0300

    added hardhat support

---
+++ hardhat.config.js
@@ -0,0 +1,13 @@
+const accounts = process.env.SIGNING_KEY ? [process.env.SIGNING_KEY] : undefined
+
+module.exports = {
+  defaultNetwork: "cotiv2",
+  solidity: "0.8.24",
+  networks: {
+    cotiv2: {
+      url: "https://node.sodalabs.net",
+      chainId: 50505050,
+      accounts,
+    },
+  },
+}
