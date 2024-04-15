59a26af9810cad18ba7c8dff61187ad7c0e527c2
vladi-coti <vladi@coti.io>
2024-04-15 15:49:41 +0300

    added nft set uri tests

--- test/confidential-nft.test.ts
+++ test/confidential-nft.test.ts
@@ -73,12 +73,12 @@ describe("Confidential NFT", function () {
       const { contract, otherAccount } = deployment
 
       const tx = await contract.connect(otherAccount).mint(otherAccount.address, { gasLimit })
-      let failed = false
+      let reverted = false
       try {
         await tx.wait()
-        failed = true
+        reverted = false
       } catch (error) {}
-      expect(failed).to.eq(false, "Should have failed")
+      expect(reverted).to.eq(true, "Should have reverted")
     })
   })
 
@@ -94,7 +94,7 @@ describe("Confidential NFT", function () {
       await (
         await contract.connect(owner).transferFrom(owner.address, otherAccount.address, tokenId, { gasLimit })
       ).wait()
-      console.log('transferred')
+      console.log("transferred")
       // expect(await contract.ownerOf(tokenId)).to.equal(otherAccount.address)
     })
 
@@ -104,13 +104,15 @@ describe("Confidential NFT", function () {
       const tokenId = await deployment.contract.totalSupply()
       await (await contract.connect(owner).mint(owner.address, { gasLimit })).wait()
 
-      const tx = await contract.connect(otherAccount).transferFrom(owner.address, otherAccount.address, tokenId, { gasLimit })
-      let failed = false
+      const tx = await contract
+        .connect(otherAccount)
+        .transferFrom(owner.address, otherAccount.address, tokenId, { gasLimit })
+      let reverted = false
       try {
         await tx.wait()
-        failed = true
+        reverted = false
       } catch (error) {}
-      expect(failed).to.eq(false, "Should have failed")
+      expect(reverted).to.eq(true, "Should have reverted")
     })
 
     it("Should fail to transfer from non-owner", async function () {
@@ -119,20 +121,84 @@ describe("Confidential NFT", function () {
       const tokenId = await deployment.contract.totalSupply()
       await (await contract.connect(owner).mint(owner.address, { gasLimit })).wait()
 
-
-      const tx = await contract.connect(otherAccount).transferFrom(owner.address, otherAccount.address, tokenId, { gasLimit })
-      let failed = false
+      const tx = await contract
+        .connect(otherAccount)
+        .transferFrom(owner.address, otherAccount.address, tokenId, { gasLimit })
+      let reverted = false
       try {
         await tx.wait()
-        failed = true
+        reverted = false
       } catch (error) {}
-      expect(failed).to.eq(false, "Should have failed")
+      expect(reverted).to.eq(true, "Should have reverted")
 
       // await expect(
       //   (
       //     await contract.connect(otherAccount).transferFrom(owner.address, otherAccount.address, tokenId, { gasLimit })
       //   ).wait()
       // ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved")
+    })
+  })
+
+  describe("URI", function () {
+    it("should return empty string for token URI if not set", async function () {
+      const { contract, owner } = deployment
+
+      const tokenId = 0
+      const ctURI = await contract.connect(owner).tokenURI(tokenId)
+      const uri = decryptValue(ctURI)
+      expect(uri).to.equal(0)
+    })
+
+    it("should allow owner to set token URI", async function () {
+      const { contract, contractAddress, owner } = deployment
+
+      const tokenId = 0
+      const uri = 11
+
+      const func = contract.connect(owner).setTokenURI
+      const selector = func.fragment.selector
+      let { ctInt, signature } = await prepareIT(BigInt(uri), owner, contractAddress, selector)
+      await (await func(tokenId, ctInt, signature, { gasLimit })).wait()
+
+      const ctRetrievedUri = await contract.tokenURI(tokenId)
+      expect(decryptValue(ctRetrievedUri)).to.equal(uri)
+    })
+
+    it("should revert when non-owner tries to set token URI", async function () {
+      const { contract, contractAddress, otherAccount } = deployment
+
+      const tokenId = 0
+      const uri = 11
+
+      const func = contract.connect(otherAccount).setTokenURI
+      const selector = func.fragment.selector
+      let { ctInt, signature } = await prepareIT(BigInt(uri), otherAccount, contractAddress, selector)
+
+      const tx = await func(tokenId, ctInt, signature, { gasLimit })
+      let reverted = false
+      try {
+        await tx.wait()
+        reverted = false
+      } catch (error) {}
+      expect(reverted).to.eq(true, "Should have reverted")
+    })
+
+    it("should emit URISet event on setting token URI", async function () {
+      const { contract, contractAddress, owner } = deployment
+
+      const tokenId = 0
+      const uri = 11
+
+      const func = contract.connect(owner).setTokenURI
+      const selector = func.fragment.selector
+      let { ctInt, signature } = await prepareIT(BigInt(uri), owner, contractAddress, selector)
+
+      const ctRetrievedUri = await contract.tokenURI(tokenId)
+      expect(decryptValue(ctRetrievedUri)).to.equal(uri)
+
+      await expect((await func(tokenId, ctInt, signature, { gasLimit })).wait())
+        .to.emit(contract, "MetadataUpdate")
+        .withArgs(tokenId)
     })
   })
 })
