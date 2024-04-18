78c3f9a9764dc7bb6eb7299e5cb3a45cfcf2a8fd
vladi-coti <vladi@coti.io>
2024-04-18 17:33:45 +0300

    forge testing

--- test-forge/ConfidentialERC20.t.sol
+++ test-forge/ConfidentialERC20.t.sol
@@ -2,12 +2,21 @@
 pragma solidity ^0.8.19;
 
 import {Test, console} from "forge-std/Test.sol";
+import {TestWrapper} from "./TestWrapper.sol";
 import {ERC20Example} from "../contracts/examples/ERC20Example.sol";
 
-contract ConfidentialERC20Test is Test {
+contract ConfidentialERC20Test is TestWrapper {
     ERC20Example public token;
 
     function setUp() public {
+        setupFork();
+        setupAccounts();
+
         token = new ERC20Example("Test Token", "TST", 100000000);
+    }
+
+    function test_deploy_name() view public {
+        console.log("token.name()", token.name());
+        assertEq(token.name(), "Test Token");
     }
 }
