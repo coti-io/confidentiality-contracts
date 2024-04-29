d62bbac25c0a4bae0c7c973b939c3edfe20511b8
Guy Mesika <guy@coti.io>
2024-04-29 22:01:41 +0300

    more data-on-chain examples (python sdk commit 58a896fa539b55217f56e0a3e7be43d721ea84df)

--- contracts/examples/DataOnChain.sol
+++ contracts/examples/DataOnChain.sol
@@ -6,7 +6,9 @@ contract DataOnChain {
 
     uint64 private clearValue;
     ctUint64 private ctUserSomeEncryptedValue;
+    ctUint64 private ctUserSomeEncryptedValueEncryptedInput;
     ctUint64 private ctNetworkSomeEncryptedValue;
+    ctUint64 private ctNetworkSomeEncryptedValueEncryptedInput;
 
     constructor () {
         clearValue = 5;
@@ -14,26 +16,48 @@ contract DataOnChain {
 
     event UserEncryptedValue(address indexed _from, ctUint64 ctUserSomeEncryptedValue);
 
-    function getNetworkSomeEncryptedValueOf() external view returns (ctUint64 ctSomeEncryptedValue) {
+    function getNetworkSomeEncryptedValue() external view returns (ctUint64 ctSomeEncryptedValue) {
         return ctNetworkSomeEncryptedValue;
     }
 
-    function getUserSomeEncryptedValueOf() external view returns (ctUint64 ctSomeEncryptedValue) {
+    function getNetworkSomeEncryptedValueEncryptedInput() external view returns (ctUint64 ctSomeEncryptedValue) {
+        return ctNetworkSomeEncryptedValueEncryptedInput;
+    }
+
+    function getUserSomeEncryptedValue() external view returns (ctUint64 ctSomeEncryptedValue) {
         return ctUserSomeEncryptedValue;
     }
 
-    function setSomeEncryptedValueOf(uint64 _value) external {
+    function getUserSomeEncryptedValueEncryptedInput() external view returns (ctUint64 ctSomeEncryptedValue) {
+        return ctUserSomeEncryptedValueEncryptedInput;
+    }
+
+    function setSomeEncryptedValue(uint64 _value) external {
         gtUint64 gtNetworkSomeEncryptedValue = MpcCore.setPublic64(_value);
         ctNetworkSomeEncryptedValue = MpcCore.offBoard(gtNetworkSomeEncryptedValue);
     }
 
-    function setUserSomeEncryptedValueOf() external {
+    function setSomeEncryptedValueEncryptedInput(ctUint64 _itCT, bytes calldata _itSignature) external {
+        itUint64 memory it;
+        it.ciphertext = _itCT;
+        it.signature = _itSignature;
+        gtUint64 gtNetworkSomeEncryptedValue = MpcCore.validateCiphertext(it);
+        ctNetworkSomeEncryptedValueEncryptedInput = MpcCore.offBoard(gtNetworkSomeEncryptedValue);
+    }
+
+    function setUserSomeEncryptedValue() external {
         gtUint64 gtNetworkSomeEncryptedValue = MpcCore.onBoard(ctNetworkSomeEncryptedValue);
         ctUserSomeEncryptedValue = MpcCore.offBoardToUser(gtNetworkSomeEncryptedValue, msg.sender);
         emit UserEncryptedValue(msg.sender, ctUserSomeEncryptedValue);
     }
 
-    function someValueOf() external returns (uint64 value) {
+    function setUserSomeEncryptedValueEncryptedInput() external {
+        gtUint64 gtEncryptedUserSomeEncryptedValue = MpcCore.onBoard(ctNetworkSomeEncryptedValueEncryptedInput);
+        ctUserSomeEncryptedValueEncryptedInput = MpcCore.offBoardToUser(gtEncryptedUserSomeEncryptedValue, msg.sender);
+        emit UserEncryptedValue(msg.sender, ctUserSomeEncryptedValueEncryptedInput);
+    }
+
+    function getSomeValue() external returns (uint64 value) {
         return clearValue;
     }
 }
